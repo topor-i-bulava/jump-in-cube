a43fddc39e26d45658cb2d3f19c01fb235b4e4bd
Neckrat <dangerfish547@gmail.com>
2022-10-14 19:41:02 +0300

    m

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void startGame() {
+        SceneManager.LoadScene("Level1");
+    }
+}
